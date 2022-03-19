609235e59b82493291e72e7110846f58798127e8
Yordan Koychev <51037200+yordanko@users.noreply.github.com>
2022-03-19 15:45:07 -0400

    It is done
    It is done.

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+member: Member
+  constructor(private membersService: MembersService, private activatedRouter: ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    this.getMember(this.activatedRouter.snapshot.params['username'].toLowerCase())
   }
 
+  getMember( username:string){
+    this.membersService.getMember(username).subscribe(member =>{     
+      this.member = member;
+    })
+  }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+members: Member[];
 
-  constructor() { }
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.getMembers()
   }
 
+  getMembers(){
+    this.memberService.getMembers().subscribe(members=>{
+      members.forEach(member=>{
+        member.username = member.username[0].toUpperCase()+ member.username.substring(1)
+      })
+      this.members = members;
+    });
+  }
 }
